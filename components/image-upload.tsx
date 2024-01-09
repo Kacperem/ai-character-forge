d85977958a0fd96f57c2dffd5e71f39965ea9038
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2024-01-09 17:54:21 +0100

    feat: character creation API

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -27,24 +27,24 @@ export const ImageUpload = ({
 
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
-        <CldUploadButton
-            onUpload={(result: any) => onChange(result.info.secure_url)}
-            options={{
-                maxFiles: 1
-            }}
-            uploadPreset="j65p08zy"
-            >
-            <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
-                <div className="relative h-40 w-40">
-                    <Image
-                        fill
-                        alt="Upload"
-                        src={value || "/placeholder.svg"}
-                        className="rounded-lg object-cover"
-                    />
-                </div>
-            </div>
-        </CldUploadButton>
+      <CldUploadButton
+        onUpload={(result: any) => onChange(result.info.secure_url)}
+        options={{
+          maxFiles: 1,
+        }}
+        uploadPreset="j65p08zy"
+      >
+        <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
+          <div className="relative h-40 w-40">
+            <Image
+              fill
+              alt="Upload"
+              src={value || "/placeholder.svg"}
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
+      </CldUploadButton>
     </div>
-  )
+  );
 };
